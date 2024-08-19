f1f5ffa67d3e7e6e6ae20d1b41ffe06f3a0e081c
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2024-08-20 01:10:40 +0530

    Update settings.py

--- user_management/settings.py
+++ user_management/settings.py
@@ -141,6 +141,7 @@ ALLOWED_HOSTS = ['connectingngo.onrender.com', '0.0.0.0']
 
 STATIC_URL = 'users/static/'
 #MEDIA_URL="/media/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
