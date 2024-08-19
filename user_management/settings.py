ba7e4d662db4f3ca507058ad1d9d2e749f5e5469
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2024-08-19 23:43:10 +0530

    Add files via upload

--- user_management/settings.py
+++ user_management/settings.py
@@ -136,6 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+ALLOWED_HOSTS = ['user_management.vercel.app', 'localhost', '127.0.0.1']
+
+
 STATIC_URL = '/static/'
 #MEDIA_URL="/media/"
 MEDIA_ROOT = "/media/"
